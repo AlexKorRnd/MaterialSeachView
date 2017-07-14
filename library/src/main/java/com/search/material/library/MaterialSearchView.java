0508c869406d0b0d5924b250dfc10371f406d8ee
a.korchagin <a.korchagin@growapp.biz>
2017-07-14 12:31:50 +0300

    fix bug with showing cardView when empty recyclerView for android before lollipop

--- library/src/main/java/com/search/material/library/MaterialSearchView.java
+++ library/src/main/java/com/search/material/library/MaterialSearchView.java
@@ -11,6 +11,7 @@ import android.os.Build;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.speech.RecognizerIntent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
@@ -42,6 +43,7 @@ public class MaterialSearchView extends FrameLayout {
     //Views
     private View mSearchLayout;
     private View mTintView;
+    private CardView cvItems;
     private RecyclerView rvItems;
     private EditText etSearch;
     private ImageButton btnBack;
@@ -129,6 +131,7 @@ public class MaterialSearchView extends FrameLayout {
         mSearchLayout = findViewById(R.id.search_layout);
 
         mSearchTopBar = (RelativeLayout) mSearchLayout.findViewById(R.id.searchTopBar);
+        cvItems = (CardView) mSearchLayout.findViewById(R.id.cvItems);
         rvItems = (RecyclerView) mSearchLayout.findViewById(R.id.rvItems);
         etSearch = (EditText) mSearchLayout.findViewById(R.id.etSearch);
         btnBack = (ImageButton) mSearchLayout.findViewById(R.id.btnBack);
@@ -147,7 +150,7 @@ public class MaterialSearchView extends FrameLayout {
 
         initSearchView();
 
-        rvItems.setVisibility(GONE);
+        cvItems.setVisibility(GONE);
     }
 
     private void initSearchView() {
@@ -338,8 +341,8 @@ public class MaterialSearchView extends FrameLayout {
      * Call {@link #setAdapter(RecyclerView.Adapter)} before calling this.
      */
     public void showSuggestions() {
-        if (adapter != null && adapter.getItemCount() > 0 && rvItems.getVisibility() == GONE) {
-            rvItems.setVisibility(VISIBLE);
+        if (adapter != null && adapter.getItemCount() > 0 && cvItems.getVisibility() == GONE) {
+            cvItems.setVisibility(VISIBLE);
         }
     }
 
@@ -358,8 +361,8 @@ public class MaterialSearchView extends FrameLayout {
      * Dissmiss the suggestions list.
      */
     public void dismissSuggestions() {
-        if (rvItems.getVisibility() == VISIBLE) {
-            rvItems.setVisibility(GONE);
+        if (cvItems.getVisibility() == VISIBLE) {
+            cvItems.setVisibility(GONE);
         }
     }
 
